b5d827de87e0c63c3a508f99f595cef0107da04d
Julian Calle <julian0986@gmail.com>
2021-12-01 10:54:43 -0500

    configuracion server

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+#    define('URL', 'https://fenditrabajodemo.000webhostapp.com/');
+#    define('HOST', 'localhost');
+#    define('DB', 'id12406512_bolsaempleo');
+#    define('USER', 'id12406512_admin');
+#    define('PASSWORD', "1234a");
+#    define('CHARSET', 'utf8mb4');
+
+ #define('URL',       'http://empleo.fenditrabajo.com/');
+ #define('HOST',      '151.106.110.194');
+ define('URL',       '');
+ define('HOST', 'localhost');
+ define('DB',        'u230156310_empleo');
+ define('USER',      'u230156310_empleo');
+ define('PASSWORD',  '0;h8pNt+]Cx');
+ define('CHARSET',   'utf8mb4');
+?>
