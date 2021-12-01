9ed1011f80d9a7c8b2d54c5340ae24dae93ab690
Julian Calle <julian0986@gmail.com>
2021-12-01 16:58:50 -0500

    se cambia bg de nav

--- views/header/header.php
+++ views/header/header.php
@@ -18,7 +18,7 @@
       <a href="#"><img src="<?php echo constant('URL'); ?>public/logos/logo.png" width="60%"></a>
     </div>
 
-    <nav class="navbar navbar-expand-lg navbar-light text-dark bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light text-dark ">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
