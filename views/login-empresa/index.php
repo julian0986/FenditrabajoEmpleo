c7c96ec439e08662fa0f13fd821b816885e76bad
Jairo Castiblanco <jacs2604@hotmail.com>
2021-12-01 15:54:11 -0500

    se modifica el front de login empresa

--- views/login-empresa/index.php
+++ views/login-empresa/index.php
@@ -5,18 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/fenditrabajo.css">
     <title>Iniciar sesión [Empresa] - Fendipetroleo Nacional</title>
     <link rel="icon" href="<?php echo constant('URL'); ?>public/images/favicon.png" sizes="32x32">
 
     <!-- Generic -->
-    <meta name="description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta name="description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
 
     <!-- Fancy -->
-    <meta property="fancy:name" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta property="fancy:name"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
     <meta property="fancy:images" content="assets/favicon.png">
-    <meta property="fancy:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta property="fancy:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta property="fancy:category" content="Workplace">
 
     <!-- Global Canonical -->
@@ -25,8 +29,10 @@
 
     <!-- Facebook -->
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
-    <meta property="og:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta property="og:title"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta property="og:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta property="og:image" content="https://fendipetroleo.com/nuevoF/public/icon/favicon.ico">
     <meta property="og:locale" content="es_ES">
 
@@ -34,14 +40,19 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@fendinaloficial">
     <meta name="twitter:creator" content="@fendinaloficial">
-    <meta name="twitter:title" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
-    <meta name="twitter:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta name="twitter:title"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta name="twitter:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta name="twitter:image" content="https://fendipetroleo.com/nuevoF/public/icon/favicon.ico">
 
 </head>
 <style>
-    .fondo2 {
-        background-image: url("<?php echo constant('URL'); ?>public/images/FEN-03.png");
+.fondo2 {
+    background: #f0f0f0;
+    padding: 50px 0;
+    min-height: 400px;
+    /* background-image: url("<?php echo constant('URL'); ?>public/images/FEN-03.png");
         background-repeat: no-repeat !important;
         background-attachment: scroll !important;
         background-position: center center !important;
@@ -49,76 +60,107 @@
         background-size: cover !important;
         -webkit-background-size: cover !important;
         -moz-background-size: cover !important;
-        -o-background-size: cover !important;
-    }
+        -o-background-size: cover !important; */
+}
 
-    .pd-mx {
-        padding-right: 15%;
-        padding-left: 15%;
-        padding-bottom: 2%;
-        padding-top: 2%
-    }
+
+.formpanel .btn {
+    width: 71%;
+    background: #1b4573;
+    color: #fff;
+    border-radius: 0;
+    padding: 10px;
+    font-size: 16px;
+    font-weight: 700;
+    text-transform: uppercase;
+}
+
+.socialLogin {
+    margin-bottom: 20px;
+    text-align: center;
+    border-bottom: 1px solid #eee;
+    padding-bottom: 20px;
+    background-color: #fff;
+}
+
+.logemp {
+    background-color: #fff;
+}
 </style>
 
 <script type="text/javascript">
-    function myFunction() {
-        var x = document.getElementById("password");
-        if (x.type === "password") {
-            x.type = "text";
-        } else {
-            x.type = "password";
-        }
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
     }
+}
 </script>
 
 <body>
     <?php require "views/header/header.php"; ?>
     <div class="fondo2">
-        <div class="container pd-mx">
-            <div class="card" style="width: 100%; background-color: rgba(255,255,255,0.9); border-radius: 1%;">
-                <div class="card-header">
-                    <h4 class="card-title text-center">Ingresa con tu empresa</h4>
-                </div>
-                <div class="card-body text-center">
-                    <form action="<?php echo constant('URL'); ?>loginEmpresa/loginEmpresa" method="POST">
-                        <div class="form-row pd-mx">
-                            <div class="form-group col-md-12">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <ion-icon name="at-outline" style="color:black;"></ion-icon>
-                                        </span>
-                                    </div>
-                                    <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Ingrese el correo electronico" required>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-12">
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <ion-icon name="lock-closed-outline" style="color:black;"></ion-icon>
-                                        </span>
-                                    </div>
-                                    <input type="password" name="password" class="form-control" id="password" placeholder="Ingrese la contraseña" required>
-                                </div>
-                                <input type="checkbox" onclick="myFunction()"> Mostrar contraseña
-                            </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md4"></div>
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="card">
+                        <div class="card-header socialLogin">
+                            <img src="https://www.fenditrabajo.com/fenditrabajo/src/view/assets/img/logo.png"
+                                class="logo" width="50%" height="50%" alt="" />
+                            <h3>Empresa</h3>
                         </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-primary">Ingresar</button>
+                        <div class="card-body text-center">
+                            <form action="<?php echo constant('URL'); ?>loginEmpresa/loginEmpresa" method="POST">
+                                <div class="form-row pd-mx">
+                                    <div class="form-group col-md-12">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <ion-icon name="at-outline" style="color:black;"></ion-icon>
+                                                </span>
+                                            </div>
+                                            <input type="email" name="email" class="form-control" id="inputEmail"
+                                                placeholder="Ingrese el correo electronico" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <div class="input-group mb-3">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <ion-icon name="lock-closed-outline" style="color:black;">
+                                                    </ion-icon>
+                                                </span>
+                                            </div>
+                                            <input type="password" name="password" class="form-control" id="password"
+                                                placeholder="Ingrese la contraseña" required>
+                                        </div>
+                                        <input type="checkbox" onclick="myFunction()"> Mostrar contraseña
+                                    </div>
+                                </div>
+                                <div class="text-center formpanel">
+                                    <button type="submit" class="btn btn-primary">Ingresar</button>
+                                </div>
+                            </form>
+                            <br />
+                            <a style="color: #0069d9;" href="<?php echo constant('URL'); ?>recuperarEmpresa">¿Olvidó su
+                                contraseña?</a>
                         </div>
-                    </form>
-                    <br />
-                    <a style="color: #0069d9;" href="<?php echo constant('URL'); ?>recuperarEmpresa">¿Olvidó su contraseña?</a>
-                </div>
-                <div class="card-footer text-center">
-                    <p>Aun no tienes cuenta, registrate <a href="<?php echo constant('URL'); ?>registroEmpresa" style="color:#055eb0;">aqui</a></p>
+                        <div class="card-footer text-center logemp">
+                            <p>Aun no tienes cuenta, registrate <a href="<?php echo constant('URL'); ?>registroEmpresa"
+                                    style="color:#055eb0;">aqui</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="card text-center mt mb" style="border-color: transparent">
+    <!-- Codigo de csursos, se deja mientras comentado -->
+
+    <!--     <div class="card text-center mt mb" style="border-color: transparent">
         <div class="row">
             <div class="col-md-12">
                 <h1>Cursos virtuales</h1>
@@ -134,13 +176,15 @@
                                     $curso = new Curso();
                                     $curso = $row;
                                 ?>
-                                    <div class="card" style="border-color: transparent">
-                                        <img style="border-radius: 10px;" src="<?php echo constant('URL') . $curso->isotipo; ?>" class="card-img-top" width="300px" height="200px">
-                                        <div class="card-footer" style="border-color: transparent; background: transparent">
-                                            <h5 class="card-title"><?php echo $curso->nombre; ?></h5>
-                                            <a href="<?php echo $curso->link; ?>" class="btn btn-primary">Ir al Curso</a>
-                                        </div>
+                                <div class="card" style="border-color: transparent">
+                                    <img style="border-radius: 10px;"
+                                        src="<?php echo constant('URL') . $curso->isotipo; ?>" class="card-img-top"
+                                        width="300px" height="200px">
+                                    <div class="card-footer" style="border-color: transparent; background: transparent">
+                                        <h5 class="card-title"><?php echo $curso->nombre; ?></h5>
+                                        <a href="<?php echo $curso->link; ?>" class="btn btn-primary">Ir al Curso</a>
                                     </div>
+                                </div>
                                 <?php } ?>
                             </div>
                         </div>
@@ -148,8 +192,11 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="card text-center mt mb" style="border-color: transparent">
+    </div> -->
+
+    <!-- Codigo de publicidad -->
+
+    <!-- <div class="card text-center mt mb" style="border-color: transparent">
         <div class="row">
             <div class="col-md-12">
                 <h1>Nuestros anunciantes</h1>
@@ -165,9 +212,11 @@
                                     $publi = new Publicidad();
                                     $publi = $row;
                                 ?>
-                                    <div class="card" style="border-color: transparent">
-                                        <a href="<?php echo $publi->link; ?>" target="_blank"><img style="border-radius: 10px;" width="50%" src="<?php echo constant('URL') . $publi->imagen; ?>"></a>
-                                    </div>
+                                <div class="card" style="border-color: transparent">
+                                    <a href="<?php echo $publi->link; ?>" target="_blank"><img
+                                            style="border-radius: 10px;" width="50%"
+                                            src="<?php echo constant('URL') . $publi->imagen; ?>"></a>
+                                </div>
                                 <?php } ?>
                             </div>
                         </div>
@@ -175,15 +224,18 @@
                 </div>
             </div>
         </div>
-    </div>
-
+    </div> 
+ codigo publicidad fin-->
 
     <?php require "views/footer/footer.php"; ?>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 </body>
 
