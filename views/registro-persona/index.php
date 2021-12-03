4a4664d2373c5240e8a177b74eb4aff2f089a234
cesarrojas011 <cesarrojas011@hotmail.com>
2021-12-03 16:24:20 -0500

    cambio clave

--- views/registro-persona/index.php
+++ views/registro-persona/index.php
@@ -124,7 +124,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-6">
-                                        <div class="h-captcha" data-sitekey="a171ed32-f758-428d-8f81-ef9f1d3fa18e"></div>
+                                        <div class="g-recaptcha" data-sitekey="6LcTkskZAAAAAENUR6h9uG1DOABLmOu_fHZ9OQXm"></div>
                                     </div>
                                 </div>
                                 <br />
