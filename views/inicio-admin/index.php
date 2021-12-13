8c667eb773a6ec313b5509e936667dc5467b2542
Jairo Castiblanco <jacs2604@hotmail.com>
2021-12-13 09:59:09 -0500

    se agrega icono de reportes en panel de administracion

--- views/inicio-admin/index.php
+++ views/inicio-admin/index.php
@@ -5,18 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/fenditrabajo.css">
     <title>Panel de control - Fendipetroleo Nacional</title>
     <link rel="icon" href="<?php echo constant('URL'); ?>public/images/favicon.png" sizes="32x32">
 
     <!-- Generic -->
-    <meta name="description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta name="description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
 
     <!-- Fancy -->
-    <meta property="fancy:name" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta property="fancy:name"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
     <meta property="fancy:images" content="assets/favicon.png">
-    <meta property="fancy:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta property="fancy:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta property="fancy:category" content="Workplace">
 
     <!-- Global Canonical -->
@@ -25,8 +29,10 @@
 
     <!-- Facebook -->
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
-    <meta property="og:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta property="og:title"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta property="og:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta property="og:image" content="https://fendipetroleo.com/nuevoF/public/icon/favicon.ico">
     <meta property="og:locale" content="es_ES">
 
@@ -34,8 +40,10 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@fendinaloficial">
     <meta name="twitter:creator" content="@fendinaloficial">
-    <meta name="twitter:title" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
-    <meta name="twitter:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta name="twitter:title"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta name="twitter:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta name="twitter:image" content="https://fendipetroleo.com/nuevoF/public/icon/favicon.ico">
 
 </head>
@@ -46,33 +54,39 @@
         <table class="table table-bordered" style="border-color: black;">
             <tbody>
                 <tr class="text-center bg-blue">
-                    <th colspan="4">
+                    <th colspan="5">
                         <h2>Panel de Administración</h2>
                     </th>
                 </tr>
                 <tr class="text-center">
-                    <td style="width:25%">
+                    <td style="width:20%">
                         <a href="<?php echo constant('URL'); ?>crudEmpresas/inicio">
                             <h5 style="color:black">Empresas</h5>
-                            <ion-icon style="font-size: 50px; color:black" name="business-outline"></ion-icon>
+                            <i class="fas fa-building" style="font-size: 50px; color:#1b4573"></i>
                         </a>
                     </td>
-                    <td style="width:25%">
+                    <td style="width:20%">
                         <a href="<?php echo constant('URL'); ?>crudUsuarios/inicio">
                             <h5 style="color:black">Usuarios</h5>
-                            <ion-icon style="font-size: 50px; color:black" name="people-outline"></ion-icon>
+                            <i class="fas fa-user-tie" style="font-size: 50px; color:#1b4573"></i>
                         </a>
                     </td>
-                    <td style="width:25%">
+                    <td style="width:20%">
                         <a href="<?php echo constant('URL'); ?>crudPublicidad/inicio">
                             <h5 style="color:black">Publicidad</h5>
-                            <ion-icon style="font-size: 50px; color:black" name="newspaper-outline"></ion-icon>
+                            <i class="fas fa-bullhorn" style="font-size: 50px; color:#1b4573"></i>
                         </a>
                     </td>
-                    <td style="width:25%">
+                    <td style="width:20%">
                         <a href="<?php echo constant('URL'); ?>crudCursos/inicio">
                             <h5 style="color:black">Cursos</h5>
-                            <ion-icon style="font-size: 50px; color:black" name="school-outline"></ion-icon>
+                            <i class="fas fa-graduation-cap" style="font-size: 50px; color:#1b4573"></i>
+                        </a>
+                    </td>
+                    <td style="width:20%">
+                        <a href="<?php echo constant('URL'); ?>crudCursos/inicio">
+                            <h5 style="color:black">Reportes</h5>
+                            <i class="fas fa-file-csv" style="font-size: 50px; color:#1b4573"></i>
                         </a>
                     </td>
                 </tr>
@@ -81,11 +95,14 @@
     </div>
     <?php require "views/footer/footer.php"; ?>
     <script type="text/javascript" src="<?php echo constant('URL'); ?>/public/js/fenditrabajo.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 </body>
 
