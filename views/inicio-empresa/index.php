662ab37f61dab6973ed11c40c899ed2eadd76d9e
Jairo Castiblanco <jacs2604@hotmail.com>
2021-12-16 08:51:26 -0500

    se ajusta la columna de imagenes en inicio empresa

--- views/inicio-empresa/index.php
+++ views/inicio-empresa/index.php
@@ -5,17 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Inicio - Fendipetroleo Nacional</title>
     <link rel="icon" href="<?php echo constant('URL'); ?>public/images/favicon.png" sizes="32x32">
 
     <!-- Generic -->
-    <meta name="description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta name="description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
 
     <!-- Fancy -->
-    <meta property="fancy:name" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta property="fancy:name"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
     <meta property="fancy:images" content="assets/favicon.png">
-    <meta property="fancy:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta property="fancy:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta property="fancy:category" content="Workplace">
 
     <!-- Global Canonical -->
@@ -24,8 +28,10 @@
 
     <!-- Facebook -->
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
-    <meta property="og:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta property="og:title"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta property="og:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta property="og:image" content="https://fendipetroleo.com/nuevoF/public/icon/favicon.ico">
     <meta property="og:locale" content="es_ES">
 
@@ -33,8 +39,10 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@fendinaloficial">
     <meta name="twitter:creator" content="@fendinaloficial">
-    <meta name="twitter:title" content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
-    <meta name="twitter:description" content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
+    <meta name="twitter:title"
+        content="Fendipetroleo - Federación Nacional de Distribuidores de Combustibles y Energéticos">
+    <meta name="twitter:description"
+        content="La Federación Nacional de Distribuidores de Combustibles y Energéticos – Fendipetróleo, es una organización gremial, creada en 1971 para representar los intereses de los empresarios distribuidores minoristas de combustibles líquidos y gas natural vehicular, promover la sostenibilidad del sector y contribuir al desarrollo social y económico del país. ">
     <meta name="twitter:image" content="https://fendipetroleo.com/nuevoF/public/icon/favicon.ico">
 
 </head>
@@ -49,7 +57,8 @@
                         <div class="card bordercarperemp">
                             <div class="card-body row">
                                 <div class="col-md-3 align-self-center">
-                                    <img src="<?php echo constant('URL') . $this->empresa->isotipo; ?>" alt="" width="100%">
+                                    <img src="<?php echo constant('URL') . $this->empresa->isotipo; ?>" alt=""
+                                        width="100%">
                                 </div>
                                 <div class="col-md-9 align-self-center ">
                                     <h3><?php echo $this->empresa->nombre; ?></h3>
@@ -58,7 +67,8 @@
                                     <a style="color: #224b8b" href="<?php echo constant('URL'); ?>perfilEmpresa">
                                         <ion-icon name="settings-outline"></ion-icon> Editar perfil de mi empresa
                                     </a>
-                                    <a class="d-block" style="color: #224b8b;" href="<?php echo constant('URL'); ?>cambioEmpresa">
+                                    <a class="d-block" style="color: #224b8b;"
+                                        href="<?php echo constant('URL'); ?>cambioEmpresa">
                                         <ion-icon name="lock-closed-outline"></ion-icon> Cambiar contraseña
                                     </a>
                                 </div>
@@ -76,18 +86,17 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card text-center">
-                        <div class="row">
-                            <?php
+                        <?php
                             include_once 'models/entity/publicidad.php';
                             foreach ($this->publicidadB as $row) {
                                 $publi = new Publicidad();
                                 $publi = $row;
                             ?>
-                                <a href="<?php echo $publi->link; ?>" target="_blank"><img class="pd" src="<?php echo constant('URL') . $publi->imagen; ?>" width="80%"></a>
-                            <?php } ?>
-                        </div>
-
+                        <a href="<?php echo $publi->link; ?>" target="_blank"><img class="pd"
+                                src="<?php echo constant('URL') . $publi->imagen; ?>" width="70%"></a>
+                        <?php } ?>
                     </div>
+
                 </div>
 
             </div>
@@ -110,13 +119,15 @@
                                     $curso = new Curso();
                                     $curso = $row;
                                 ?>
-                                    <div class="card" style="border-color: transparent">
-                                        <img style="border-radius: 10px;" src="<?php echo constant('URL') . $curso->isotipo; ?>" class="card-img-top" width="300px" height="200px">
-                                        <div class="card-footer" style="border-color: transparent; background: transparent">
-                                            <h5 class="card-title"><?php echo $curso->nombre; ?></h5>
-                                            <a href="<?php echo $curso->link; ?>" class="btn btn-primary">Ir al Curso</a>
-                                        </div>
+                                <div class="card" style="border-color: transparent">
+                                    <img style="border-radius: 10px;"
+                                        src="<?php echo constant('URL') . $curso->isotipo; ?>" class="card-img-top"
+                                        width="300px" height="200px">
+                                    <div class="card-footer" style="border-color: transparent; background: transparent">
+                                        <h5 class="card-title"><?php echo $curso->nombre; ?></h5>
+                                        <a href="<?php echo $curso->link; ?>" class="btn btn-primary">Ir al Curso</a>
                                     </div>
+                                </div>
                                 <?php } ?>
                             </div>
                         </div>
@@ -127,11 +138,14 @@
     </div>
 
     <?php include "views/footer/footer.php"; ?>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 </body>
 
